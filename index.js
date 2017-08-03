ed9e4387b67fad191aa67bfff81964a7ef194802
Lee <likun7981@gmail.com>
2017-08-03 17:58:34 +0800

    fix: fix global path error
    when global install , the commitizen path is difference from part install, so we should check the
    path before bootstrap commitizen.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const existSync = require('fs').existsSync;
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 
+const isGlobalInstall = existSync(path.join(__dirname, './node_modules/commitizen'));
+
+
 bootstrap({
-  cliPath: path.join(__dirname, '../../node_modules/commitizen'),
+  cliPath: isGlobalInstall ? path.join(__dirname, './node_modules/commitizen') : path.join(__dirname, '../commitizen'),
   config: {
     path: 'cz-conventional-changelog-simple'
   }
